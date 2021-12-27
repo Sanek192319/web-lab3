088c6af8f5ecfe2a44e11e6a9be7bf4f61d8262b
Sergey <sereganiceman@gmail.com>
2021-12-27 15:10:00 +0200

    fix hasura

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,21 @@
   import { getMainDefinition } from "@apollo/client/utilities";
 
   function createApolloClient() {
+    const headers = {
+      "x-hasura-admin-secret": "secretkey",
+    };
     const httpLink = new HttpLink({
       uri: "https://myweblab3.herokuapp.com/v1/graphql",
+      headers,
     });
     const cache = new InMemoryCache();
     const wsLink = new WebSocketLink({
       uri: "wss://myweblab3.herokuapp.com/v1/graphql",
       options: {
         reconnect: true,
+        connectionParams: {
+          headers,
+        },
       },
     });
     const link = split(
